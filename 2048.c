54c5710f003e2720ecf72a3d9c914ce3d13d0274
A0Dev0Knight <al.dev.knight@gmail.com>
2024-02-04 15:03:51 +0200

    Added a helper file to clean the code temporarily

--- 2048.c
+++ 2048.c
@@ -1,31 +1,5 @@
 // librarii necesare
-#include <ncurses.h>
-#include <time.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-
-/*======NCURSES======*/
-// constante necesare programului mare
-#define FOREVER 1
-
-// aceasta functie apeleaza alte functii specifice ncurses, pentru un gameplay placut :)
-void initializareJoc()
-{
-    // golesc ce e pe ecran just in case
-    clear();
-    // dau refresh sa ma asigur ca incep fresh
-    refresh();
-    // opresc cursorul pt ca nu am nevoie sa il vad
-    curs_set(0);
-    // nu printez tastele pe care apas
-    noecho();
-    // trimit inputul direct la aplicatie (daca am inteles bine, inteleg ca e mai rapid inputul asa)
-    cbreak();
-}
-/*======NCURSES======*/
-
+#include "helper.h"
 
 /*======GAMEPLAY LOOP======*/
 #define PANOU_CONTROL_H 5
@@ -182,77 +156,8 @@ void generate2or4(int **tablaJoc)
     }
 }
 
-// aceasta functie copiaza toate elementele matricei de joc in alta matrice
-void CPYelement(int **matrice_sursa, int **matrice_destinatie)
-{
-    int i, j;
-    for (i = 0; i < 4; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            matrice_destinatie[i][j] = matrice_sursa[i][j];
-        }
-    }
-}
-
-typedef struct
-{
-    int score;
-    int dir;
-} AImove;
-
-// calculatorul va putea muta singur dupa un nr de secunde,
-// fiecare mutare posibila are un scor si o directie,
-// aceasta functie returneaza miscarea cu cel mai mare scor, astfel pastrand si directia 
-AImove maxim(AImove a, AImove b)
-{
-    if (a.score > b.score)
-    {
-        return a;
-    }
-    else
-        return b;
-}
-
 // prototipul functiei
 void AIhandleCommand(int **tablaJoc, int *globalScore);
-
-// verific daca inputul meu este bun
-int goodInput(int input)
-{
-    switch (input)
-    {
-    // controale miscari joc
-    case 'w':
-    case 'W':
-    case KEY_UP:
-    case 'd':
-    case 'D':
-    case KEY_RIGHT:
-    case 's':
-    case 'S':
-    case KEY_DOWN:
-    case 'a':
-    case 'A':
-    case KEY_LEFT:
-    case 'q':
-    case 'Q':
-    
-    // pause
-    case 'p':
-    case 'P':
-    
-    // undo
-    case 'u':
-    case 'U':
-        return 1;
-        break;
-    
-    default:
-        break;
-    }
-    return 0;
-}
 
 // functie ce imi numara un anumit nr de secunde si executa la final o alta functie
 int Timer(WINDOW *gameInfoWindow, WINDOW *gameWindow, int max_terminal_x, int **tablaJoc, int *globalScore)
